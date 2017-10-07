f001584984306d7d52b33ac9caf4e4bd3208bffc
Best <best@Bests-MacBook-Pro.local>
2017-10-07 23:03:23 +0700

    first commit

---
+++ src/main/java/com/blog/sonia/demospringbootprofile/DemoSpringbootProfileApplication.java
@@ -0,0 +1,12 @@
+package com.blog.sonia.demospringbootprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootProfileApplication.class, args);
+	}
+}
